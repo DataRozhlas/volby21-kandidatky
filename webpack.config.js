ac294a2f68145fa3c629c3f1c7e7bfb582f8bbc4
Petr Kočí <petr.koci@samizdat.cz>
2021-09-10 10:51:35 +0200

    builduje se, ale rve

--- webpack.config.js
+++ webpack.config.js
@@ -34,28 +34,15 @@
 //   },
 // };
 
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   resolve: {
     fallback: {
-      tls: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      stream: false,
-      crypto: false,
-      url: require.resolve("url/"),
-      util: require.resolve("util/"),
-      buffer: require.resolve("buffer/"),
-      vm: require.resolve("vm-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      constants: require.resolve("constants-browserify"),
-      assert: require.resolve("assert/"),
-      fs: require.resolve("fs/"),
-      path: require.resolve("path/"),
+      fs: false,
+      child_process: false,
+      worker_threads: false,
     },
   },
   entry: "./js/script.js",
@@ -63,7 +50,7 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: "output.js",
   },
-
+  plugins: [new NodePolyfillPlugin()],
   module: {
     rules: [
       {
